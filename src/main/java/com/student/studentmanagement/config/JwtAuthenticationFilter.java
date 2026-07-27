f20185e9840d416a56b3a6d276dccaa01eed6acf
Akash Vanageri <akashvanageri55@gmail.com>
2026-07-27 23:47:30 +0530

    Skip JWT filter for public APIs

--- src/main/java/com/student/studentmanagement/config/JwtAuthenticationFilter.java
+++ src/main/java/com/student/studentmanagement/config/JwtAuthenticationFilter.java
@@ -10,9 +10,13 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
 import org.springframework.stereotype.Component;
+
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -20,50 +24,67 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+
     private final JwtService jwtService;
+
     private final CustomUserDetailsService userDetailsService;
 
-    public JwtAuthenticationFilter(JwtService jwtService,
-                                   CustomUserDetailsService userDetailsService) {
+
+    public JwtAuthenticationFilter(
+            JwtService jwtService,
+            CustomUserDetailsService userDetailsService) {
+
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
     }
 
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+
+        String path = request.getServletPath();
+
+        return path.startsWith("/auth/")
+                || path.startsWith("/students/")
+                || path.equals("/");
+    }
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+
             throws ServletException, IOException {
 
-        System.out.println("==================================");
 
         String authHeader = request.getHeader("Authorization");
 
-        System.out.println("Authorization Header = " + authHeader);
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            System.out.println("Bearer token not found");
+        if (authHeader == null ||
+                !authHeader.startsWith("Bearer ")) {
+
             filterChain.doFilter(request, response);
             return;
         }
+
 
         try {
 
             String token = authHeader.substring(7);
 
-            System.out.println("JWT Token = " + token);
-
             String email = jwtService.extractEmail(token);
 
-            System.out.println("Email = " + email);
 
             if (email != null &&
-                    SecurityContextHolder.getContext().getAuthentication() == null) {
+                    SecurityContextHolder.getContext()
+                            .getAuthentication() == null) {
+
 
                 UserDetails userDetails =
                         userDetailsService.loadUserByUsername(email);
 
-                System.out.println("User Loaded = " + userDetails.getUsername());
 
                 UsernamePasswordAuthenticationToken authentication =
                         new UsernamePasswordAuthenticationToken(
@@ -72,26 +93,33 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                 userDetails.getAuthorities()
                         );
 
+
                 authentication.setDetails(
                         new WebAuthenticationDetailsSource()
                                 .buildDetails(request)
                 );
 
-                SecurityContextHolder.getContext().setAuthentication(authentication);
 
-                System.out.println("Authentication Successful");
+                SecurityContextHolder.getContext()
+                        .setAuthentication(authentication);
+
             }
+
 
         } catch (Exception e) {
 
-            System.out.println("JWT ERROR");
-            e.printStackTrace();
+            response.setStatus(
+                    HttpServletResponse.SC_UNAUTHORIZED
+            );
 
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write(e.getMessage());
+            response.getWriter()
+                    .write("Invalid JWT Token");
+
             return;
         }
 
+
         filterChain.doFilter(request, response);
     }
+}
 }
